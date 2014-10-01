ef76a1b91cb38ccccaeb67cc384423b3f1217b8b
Sebastien Binet <binet@cern.ch>
2014-10-01 15:48:49 +0200

    vego: references are now in README

--- vego.go
+++ vego.go
@@ -1,7 +1,2 @@
 // Package vego is a pure-go implementation of Vega, a grammar of graphics.
-//
-// See:
-//  https://github.com/trifacta/vega/wiki/Tutorial
-//  https://github.com/wrobstory/vincent
-//  http://bokeh.pydata.org/index.html
 package vego
